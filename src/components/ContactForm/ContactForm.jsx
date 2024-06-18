401626fb899d8e0cf7fb665ee0eb41a7c31ca3be
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-06-19 00:38:42 +0300

    2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { nanoid } from 'nanoid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'Yup';
+import * as Yup from 'yup';
 
 const userSchema = Yup.object().shape({
   name: Yup.string()
